8c97aad6217697d7a512a59528f7b84d8532a4d6
yosefkahlon <yosefkahlon53@gmail.com>
2022-04-03 19:18:31 +0300

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Matrix_Calculator_part_A.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Matrix_Calculator_part_A.dir/main.cpp.o"
+  "Matrix_Calculator_part_A"
+  "Matrix_Calculator_part_A.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Matrix_Calculator_part_A.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
